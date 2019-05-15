0e84bcc89fec63f5f4f8a01e6ba742ba75ddcbaf
jhonatan.leon <jhonatan.leon@farmatodo.com>
2019-05-15 00:13:14 -0500

    se termina servicio de twwiter

---
+++ server.js
@@ -0,0 +1,33 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+    consumer_key: 'VUphm8lWqpITuuQ2ANzxqRY0t',
+    consumer_secret: 'IIyfpC3iHzX3NBVItOdq6C8mEdpkeewg7XDgcKcOv9MwBzGhnL',
+    access_token_key: '1080939291499479040-xJz8lts10lBaZZqx3eusw5zp53lOsa',
+    access_token_secret: 'aQbgppmSimiPUYG4TxdW4WNLrnAD153rZ5Da5OtoP8fgu'
+});
+
+var params = { screen_name: 'nodejs' };
+
+
+var express = require('express')
+var app = express()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/tw', function(req, res) {
+    let count = req.query.count;
+    let next = req.query.next;
+    let query = req.query.query;
+
+    client.get('search/tweets', { q: query, count: count, result_type: 'popular', max_id: next, lang: 'es' }, function(error, tweets, response) {
+        res.json(tweets);
+    });
+
+})
+
+app.listen(3000)
